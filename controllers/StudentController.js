c1217bb1104c5bf55c7f77d1c46e3281be312f7a
Ahmad Fauzan <57349734+FauzanKatil@users.noreply.github.com>
2021-12-31 09:55:41 +0700

    Initial commit

---
+++ controllers/StudentController.js
@@ -0,0 +1,75 @@
+// import Model Student
+const bodyParser = require("body-parser");
+const { append } = require("express/lib/response");
+const Student = require("../models/Student");
+
+class StudentController {
+  // menambahkan keyword async
+  async index(req, res) {
+    // memanggil method static all dengan async await.
+    const students = await Student.all();
+
+    const data = {
+      message: "Menampilkkan semua students",
+      data: students,
+    };
+
+    res.json(data);
+  }
+
+  async store(req, res) {
+    /**
+     * TODO 2: memanggil method create.
+     * Method create mengembalikan data yang baru diinsert.
+     * Mengembalikan response dalam bentuk json.
+     */
+    // code here
+    const { name } = req.body;
+    const { nim } = req.body;
+    const { prodi } = req.body;
+    const { address } = req.body;
+
+    const insert_data = [
+      name,
+      nim,
+      prodi,
+      address
+    ];
+    const insert = Student.create(name, nim, prodi, address); 
+    const data = {
+      message: "Menambahkan data student",
+      data: insert,
+    };
+
+    res.json(data);
+  }
+
+  update(req, res) {
+    const { id } = req.params;
+    const { nama } = req.body;
+
+    const data = {
+      message: `Mengedit student id ${id}, nama ${nama}`,
+      data: [],
+    };
+
+    res.json(data);
+  }
+
+  destroy(req, res) {
+    const { id } = req.params;
+
+    const data = {
+      message: `Menghapus student id ${id}`,
+      data: [],
+    };
+
+    res.json(data);
+  }
+}
+
+// Membuat object StudentController
+const object = new StudentController();
+
+// Export object StudentController
+module.exports = object;
